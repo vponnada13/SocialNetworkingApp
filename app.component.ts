b7b007f050ba79c6471ef8297232ec244c8e5e29
vponnada13 <97481719+vponnada13@users.noreply.github.com>
2024-05-17 20:10:51 -0700

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users:any;
+  constructor(private http:HttpClient) {
+    
+  }
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next:response=>this.users=response,
+      error:() =>console.log(error),
+      complete:() =>{'Request has completed'}
+    })
+  }
+  
+}
